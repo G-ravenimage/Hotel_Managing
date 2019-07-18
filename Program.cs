d1267fde671a25369cc7430f3635a3d7021c3e54
G-ravenimage <52974177+G-ravenimage@users.noreply.github.com>
2019-07-18 15:38:23 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Hotel_Managing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
